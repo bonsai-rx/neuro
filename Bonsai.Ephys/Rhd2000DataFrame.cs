4a7049f19b286369c940e5eabcf5c8a0cfd25392
glopes <glopes@localhost>
2014-03-19 02:08:33 +0000

    Updated Rhd2000DataFrame to output raw unscaled ADC and TTL data.

--- Bonsai.Ephys/Rhd2000DataFrame.cs
+++ Bonsai.Ephys/Rhd2000DataFrame.cs
@@ -12,26 +12,51 @@ namespace Bonsai.Ephys
         public Rhd2000DataFrame(Rhd2000DataBlock dataBlock, double bufferCapacity)
         {
             Timestamp = dataBlock.Timestamp;
-            AmplifierData = GetStreamData(dataBlock.AmplifierData, 0.195, -6389.76);
-            AuxiliaryData = GetStreamData(dataBlock.AuxiliaryData, 0.0000374, 0);
-            BoardAdcData = Mat.FromArray(dataBlock.BoardAdcData);
-            TtlIn = Mat.FromArray(dataBlock.TtlIn);
-            TtlOut = Mat.FromArray(dataBlock.TtlOut);
+            AmplifierData = GetStreamData(dataBlock.AmplifierData);
+            AuxiliaryData = GetStreamData(dataBlock.AuxiliaryData);
+            BoardAdcData = GetAdcData(dataBlock.BoardAdcData);
+            TtlIn = GetTtlData(dataBlock.TtlIn);
+            TtlOut = GetTtlData(dataBlock.TtlOut);
             BufferCapacity = bufferCapacity;
         }
 
-        Mat GetStreamData(int[][,] data, double scale, double shift)
+        Mat GetTtlData(int[] data)
+        {
+            var output = new Mat(1, data.Length, Depth.U8, 1);
+            using (var header = Mat.CreateMatHeader(data))
+            {
+                CV.Convert(header, output);
+            }
+
+            return output;
+        }
+
+        Mat GetAdcData(int[,] data)
+        {
+            var numChannels = data.GetLength(0);
+            var numSamples = data.GetLength(1);
+
+            var output = new Mat(numChannels, numSamples, Depth.U16, 1);
+            using (var header = Mat.CreateMatHeader(data))
+            {
+                CV.Convert(header, output);
+            }
+
+            return output;
+        }
+
+        Mat GetStreamData(int[][,] data)
         {
             var numChannels = data[0].GetLength(0);
             var numSamples = data[0].GetLength(1);
 
-            var output = new Mat(data.Length * numChannels, numSamples, Depth.F32, 1);
+            var output = new Mat(data.Length * numChannels, numSamples, Depth.U16, 1);
             for (int i = 0; i < data.Length; i++)
             {
                 using (var header = Mat.CreateMatHeader(data[i]))
                 using (var subRect = output.GetSubRect(new Rect(0, i * numChannels, numSamples, numChannels)))
                 {
-                    CV.ConvertScale(header, subRect, scale, shift);
+                    CV.Convert(header, subRect);
                 }
             }
 
