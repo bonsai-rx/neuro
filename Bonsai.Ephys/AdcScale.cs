0755473758f9bffee42d69d2c0670b8c84d70288
glopes <glopes@localhost>
2014-03-19 02:10:54 +0000

    Added AdcScale module to Ephys package to provide unit scaling for known ADCs.

---
+++ Bonsai.Ephys/AdcScale.cs
@@ -0,0 +1,45 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Ephys
+{
+    public class AdcScale : Transform<Mat, Mat>
+    {
+        public AdcType AdcType { get; set; }
+
+        public override IObservable<Mat> Process(IObservable<Mat> source)
+        {
+            return source.Select(input =>
+            {
+                var output = new Mat(input.Size, Depth.F32, input.Channels);
+                switch (AdcType)
+                {
+                    case AdcType.Electrode:
+                        CV.ConvertScale(input, output, 0.195, -6389.76);
+                        break;
+                    case AdcType.AuxiliaryInput:
+                        CV.ConvertScale(input, output, 0.0000374, 0);
+                        break;
+                    case AdcType.SupplyVoltage:
+                        CV.ConvertScale(input, output, 0.0000748, 0);
+                        break;
+                    case AdcType.Temperature:
+                        CV.ConvertScale(input, output, 1 / 100.0, 0);
+                        break;
+                    case AdcType.BoardAdc:
+                        CV.ConvertScale(input, output, 0.000050354, 0);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Invalid adc type.");
+                }
+
+                return output;
+            });
+        }
+    }
+}
