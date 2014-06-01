0fd4b4b4ab46cc5800cc554d71c6b41d00208a27
glopes <glopes@localhost>
2014-06-01 17:11:10 +0100

    Cleanup and simplification of flyPAD data acquisition package.

--- Bonsai.FlyPad/FlyPadSource.cs
+++ Bonsai.FlyPad/FlyPadSource.cs
@@ -6,34 +6,68 @@ using OpenCV.Net;
 using System.Reactive.Linq;
 using System.Threading;
 using System.ComponentModel;
+using FTD2XX_NET;
 
 namespace Bonsai.FlyPad
 {
     public class FlyPadSource : Source<Mat>
     {
+        const string StartCommand = "G";
+        const string StopCommand = "S";
         const int ChannelCount = 64;
         const int FrameSize = 640;
-        FTDIReader source = new FTDIReader();
+        FTDI source = new FTDI();
 
         [TypeConverter(typeof(PortNameConverter))]
         public string PortName { get; set; }
 
         public int BaudRate { get; set; }
 
+        private static void Write(FTDI source, string command)
+        {
+            var bytesWritten = 0u;
+            var result = source.Write(command, command.Length, ref bytesWritten);
+            if (result != FTDI.FT_STATUS.FT_OK)
+            {
+                throw new InvalidOperationException("Unable to write command to the FTDI device.");
+            }
+        }
+
+        private static void Read(FTDI source, byte[] bytes)
+        {
+            var numBytesAvailable = 0u;
+            var result = source.Read(bytes, (uint)bytes.Length, ref numBytesAvailable);
+            if (result != FTDI.FT_STATUS.FT_OK)
+            {
+                throw new InvalidOperationException("Failed to read data frame from the FTDI device.");
+            }
+        }
+
         public override IObservable<Mat> Generate()
         {
             return Observable.Create<Mat>(observer =>
             {
                 var running = true;
-                source.Open(PortName, BaudRate);
-                source.Send("G");
+                var status = source.OpenBySerialNumber(PortName);
+                if (status != FTDI.FT_STATUS.FT_OK)
+                {
+                    throw new InvalidOperationException("Unable to open the FTDI device at the specified serial port.");
+                }
+
+                status = source.SetTimeouts(5000, 1000);
+                if (status != FTDI.FT_STATUS.FT_OK)
+                {
+                    throw new InvalidOperationException("Unable to set timeouts on the FTDI device.");
+                }
+
+                Write(source, StartCommand);
                 var thread = new Thread(() =>
                 {
                     var dataFrame = new short[ChannelCount, 1];
                     var packet = new byte[FrameSize];
                     while (running)
                     {
-                        source.Read(packet);
+                        Read(source, packet);
                         for (int i = 0; i < packet.Length; i += 20)
                         {
                             var channelOffset = (i / 20) * 2;
@@ -61,7 +95,7 @@ namespace Bonsai.FlyPad
                 {
                     running = false;
                     if (thread != Thread.CurrentThread) thread.Join();
-                    source.Send("S");
+                    Write(source, StopCommand);
                     source.Close();
                 };
             });
@@ -76,8 +110,18 @@ namespace Bonsai.FlyPad
 
             public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
             {
-                var flypad = (FlyPadSource)context.Instance;
-                return new StandardValuesCollection(flypad.source.PortsAvailable);
+                string[] portNames;
+                var numberOfDevices = 0u;
+                var source = ((FlyPadSource)context.Instance).source;
+                source.GetNumberOfDevices(ref numberOfDevices);
+                var deviceList = new FTDI.FT_DEVICE_INFO_NODE[numberOfDevices];
+                var status = source.GetDeviceList(deviceList);
+                if (status == FTDI.FT_STATUS.FT_OK)
+                {
+                    portNames = Array.ConvertAll(deviceList, device => device.SerialNumber);
+                }
+                else portNames = new string[0];
+                return new StandardValuesCollection(portNames);
             }
         }
     }
