369632e93e0a7fd7a513798a4b0a529c916333a2
glopes <glopes@localhost>
2015-07-25 03:54:35 +0100

    Fixed #2. Added TtlState to split packed digital inputs into multi-channels.

---
+++ Bonsai.Ephys/TtlState.cs
@@ -0,0 +1,29 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Ephys
+{
+    public class TtlState : Transform<Mat, Mat>
+    {
+        public override IObservable<Mat> Process(IObservable<Mat> source)
+        {
+            return source.Select(input =>
+            {
+                var output = new Mat(8, input.Cols, Depth.U8, 1);
+                for (int i = 0; i < output.Rows; i++)
+                {
+                    using (var row = output.GetRow(i))
+                    {
+                        CV.AndS(input, Scalar.Real(1 << i), row);
+                    }
+                }
+                return output;
+            });
+        }
+    }
+}
