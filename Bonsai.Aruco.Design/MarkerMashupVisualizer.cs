b8e5e2047f9894d79b04820cbbe6b4020ecf11c7
glopes <glopes@localhost>
2015-09-12 21:27:55 +0100

    Added marker mashup visualizer.

---
+++ Bonsai.Aruco.Design/MarkerMashupVisualizer.cs
@@ -0,0 +1,40 @@
+﻿using Aruco.Net;
+using Bonsai;
+using Bonsai.Aruco.Design;
+using Bonsai.Design;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: TypeVisualizer(typeof(MarkerMashupVisualizer), Target = typeof(VisualizerMashup<IplImageVisualizer, MarkerVisualizer>))]
+
+namespace Bonsai.Aruco.Design
+{
+    public class MarkerMashupVisualizer : MashupTypeVisualizer
+    {
+        IplImageVisualizer visualizer;
+
+        public override void Show(object value)
+        {
+            var marker = (Marker)value;
+            var visualizerImage = visualizer.VisualizerImage;
+            if (visualizerImage != null && marker != null && marker.IsValid)
+            {
+                marker.Draw(visualizerImage, Scalar.Rgb(0, 0, 255), 2, true);
+            }
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            visualizer = (IplImageVisualizer)provider.GetService(typeof(DialogMashupVisualizer));
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
---
+++ Bonsai.Aruco.Design/MarkerVisualizer.cs
@@ -0,0 +1,18 @@
+﻿using Aruco.Net;
+using Bonsai;
+using Bonsai.Aruco.Design;
+using Bonsai.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: TypeVisualizer(typeof(MarkerVisualizer), Target = typeof(Marker))]
+
+namespace Bonsai.Aruco.Design
+{
+    public class MarkerVisualizer : ObjectTextVisualizer
+    {
+    }
+}
