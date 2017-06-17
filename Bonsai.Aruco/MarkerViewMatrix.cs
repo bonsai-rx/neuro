f5ce67df1317f55b00e64882d35b1e8314a3f784
glopes <glopes@localhost>
2017-06-17 15:24:07 +0100

    Updated extrinsic parameter extraction to avoid requiring a projection matrix.

--- Bonsai.Aruco/MarkerViewMatrix.cs
+++ Bonsai.Aruco/MarkerViewMatrix.cs
@@ -2,6 +2,7 @@
 using OpenTK;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
@@ -9,34 +10,24 @@ using System.Threading.Tasks;
 
 namespace Bonsai.Aruco
 {
-    public class MarkerProjection : Transform<Marker, Matrix4>
+    [Description("Returns the modelview matrix given the extrinsic parameters of the specified input marker.")]
+    public class MarkerViewMatrix : Transform<Marker, Matrix4>
     {
-        public float FovY { get; set; }
-
-        public float AspectRatio { get; set; }
-
-        public float NearClip { get; set; }
-
-        public float FarClip { get; set; }
-
         public override IObservable<Matrix4> Process(IObservable<Marker> source)
         {
             return source.Select(input =>
             {
-                var angle = MathHelper.DegreesToRadians(FovY);
-                var projection = Matrix4.CreatePerspectiveFieldOfView(angle, AspectRatio, NearClip, FarClip);
-                var modelView = default(Matrix4);
                 if (input.IsValid)
                 {
                     var extrinsics = input.GetGLModelViewMatrix();
-                    modelView = new Matrix4(
+                    var modelView = new Matrix4(
                         (float)extrinsics[0], (float)extrinsics[1], (float)extrinsics[2], (float)extrinsics[3],
                         (float)extrinsics[4], (float)extrinsics[5], (float)extrinsics[6], (float)extrinsics[7],
                         (float)extrinsics[8], (float)extrinsics[9], (float)extrinsics[10], (float)extrinsics[11],
                         (float)extrinsics[12], (float)extrinsics[13], (float)extrinsics[14], (float)extrinsics[15]);
-                    modelView = modelView * Matrix4.CreateScale(1, -1, 1);
+                    return modelView * Matrix4.CreateScale(1, -1, 1);
                 }
-                return modelView * projection;
+                return default(Matrix4);
             });
         }
     }
--- Bonsai.Aruco/Properties/AssemblyInfo.cs
+++ Bonsai.Aruco/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gonçalo Lopes")]
 [assembly: AssemblyProduct("Bonsai.Aruco")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2012-2014")]
+[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2012-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.0")]
